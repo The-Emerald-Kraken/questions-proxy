ac2d5d4a3df67e97b76e1a861cac9e0573982852
Joe Redmon <joesredmon@gmail.com>
2020-10-06 16:55:06 -0700

    server side rendering

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -7,9 +7,24 @@ const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const PORT = 3000;
 app.use(express.static(PUBLIC_DIR));
 
-app.get('api/products/questions/sort/:sort', (req, res) => {
-  res.redirect(`http://localhost:3001/bundle.js${req.baseUrl}`);
-  console.log('success');
+app.get('/api/products/questions/sort/:sort', (req, res) => {
+  res.redirect(`http://localhost:3001${req.url}`);
+});
+
+app.get('/api/products/reviews/:id/:count/:sort', (req, res) => {
+  res.redirect(`http://localhost:3005${req.url}`);
+});
+
+app.get('/api/products/reviews/avg/:item', (req, res) => {
+  res.redirect(`http://localhost:3005${req.url}`);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  res.redirect(`http://localhost:3002${req.url}`);
+});
+
+app.get('/api/products/:id/relatedItems', (req, res) => {
+  res.redirect(`http://localhost:3004${req.url}`);
 });
 
 app.listen(PORT, () => {
